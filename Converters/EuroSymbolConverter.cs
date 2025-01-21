0cd77c61c60860b4d74e32636d27397f90fb4f89
shypy <vlad.sipos@ymail.com>
2025-01-21 12:11:35 +0200

    Credits page simpla, doar cu valorile de credite si baza de date pentru credite.

---
+++ Converters/EuroSymbolConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProoiectVladSipos.Converters
+{
+    public class EuroSymbolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Adaugă simbolul € pentru afișare
+            if (value is decimal decimalValue)
+            {
+                return $"{decimalValue:0.00} €";
+            }
+            if (value is double doubleValue)
+            {
+                return $"{doubleValue:0.00} €";
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Elimină simbolul € pentru salvare
+            if (value is string stringValue)
+            {
+                stringValue = stringValue.Replace("€", "").Trim();
+                if (decimal.TryParse(stringValue, out var result))
+                {
+                    return result;
+                }
+            }
+            return 0;
+        }
+    }
+}
